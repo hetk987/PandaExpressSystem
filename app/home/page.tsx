165cd076f4b466c823dd65837b81501c5e152b6c
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-10-31 09:04:11 -0500

    feature: added option grid

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,13 @@
+import MealCard from "../components/app-mealcard";
+
+const options = ['orange chicken', 'bejing beef', 'tariyaki chicken', 'tacos', 'loaf of bread']
+
 export default function Home() {
   return (
-    <div>
-        Inside
+    <div className="grid grid-cols-5 gap-10 p-10 w-full">
+        {options.map((item, i) => (
+            <MealCard name={item} image="/images/image.png" key={i}/>
+        ))}
     </div>
   );
 }
