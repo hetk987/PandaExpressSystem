6c955003768a63bf88235406a0fb589fae49da4c
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-11-03 10:02:24 -0600

    Added better mealcards

--- app/home/build/[meal]/page.tsx
+++ app/home/build/[meal]/page.tsx
@@ -5,60 +5,58 @@ import MealCard from "@/app/components/app-mealcard";
 import { useEffect } from "react";
 import { useState } from "react";
 
-const options: {
-  name: string,
-  entrees: number,
-  sides: number,
-}[] = [{
-  name: "Bowl", 
-  entrees: 1, 
-  sides: 2
-}, {
-  name: "Plate", 
-  entrees: 2, 
-  sides: 2
-}, {
-  name: "Bigger Plate", 
-  entrees: 3, 
-  sides: 2
-}]
+import { Recipe } from "@/lib/types";
+import { MealType } from "@/lib/types";
 
-export default async function Build({
+export default function Build({
     params
 }: {
-    params: Promise<{ slug: string }>
+    params: Promise<{ meal: string }>
 }) {
-    // const { slug } = use(params);
-    // const [recipes, setRecipes] = useState([]);
+    const { meal } = use(params);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await fetch(`/api/recipes`);
-    //             if (response.ok) {
-    //                 const data = await response.json();
-    //                 setRecipes(data);
-    //             }
-    //         } catch (error) {
-    //             console.error("Failed to fetch links");
-    //         } 
-    //     }
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [mealtypes, setMealtypes] = useState<MealType[]>([]);
 
-    //     fetchData();
-    // }, []);
+    // fetch recipes
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                let response = await fetch(`/api/recipes`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setRecipes(data);
+                }
+
+                response = await fetch(`/api/mealtypes`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setMealtypes(data);
+                }
+            } catch (error) {
+                console.error("Failed to fetch links");
+            } 
+        }
+
+        fetchData();
+    }, []);
+
+    const mealtype = mealtypes.find(m => m.name === meal);
 
     return (
         <div className="flex flex-row">
-            {/* <div className="grid grid-cols-4 gap-10 p-10 w-full">
+            <div className="grid grid-cols-4 gap-10 p-10 w-full mb-10">
                 {recipes.map((item, i) => (
-                    <a href="/">
+                    <a href="/" key={i}>
                         <MealCard name={item.name} image="/images/image.png" key={i}/>
                     </a>
                 ))}
             </div>
-            <div className="bg-red-50 w-100">
-                Hello
-            </div> */}
+            <div className="flex flex-col w-100 bg-red-50">
+                <p>{mealtype?.name}</p>
+                <MealCard name="Entree 1"/>
+            </div>
         </div>
     );
 }
