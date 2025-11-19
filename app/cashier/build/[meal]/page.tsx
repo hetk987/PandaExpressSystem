ff41304952dbbc741679996c77c700822d26805e
Isaac_Lagoy <isaacblagoy@gmail.com>
2025-11-19 10:08:36 -0600

    Added minimal buttons to cashier view

--- app/cashier/build/[meal]/page.tsx
+++ app/cashier/build/[meal]/page.tsx
@@ -8,6 +8,7 @@ import { useCart } from "@/app/providers/cart-provider";
 import { Button } from "@/app/components/ui/button";
 import { useRouter } from "next/navigation";
 import { useAddToCartToast } from "@/app/hooks/use-add-to-cart-toast";
+import CashierCard from "@/app/components/app-cashier-card";
 
 interface Selection {
     type: RecipeType;
@@ -178,16 +179,12 @@ export default function Build({
             }
         );
         
-        router.push("/home/build");
+        router.push("/cashier/build");
     };
 
     return (
         <div className="flex flex-row">
-            <div className="w-70">
-                <div className="pt-5 pl-5">
-                    <h2 className=" text-3xl font-bold ">{mealName}</h2>
-                </div>
-                <div className="flex flex-col p-10 gap-10 mb-10">
+            <div className="flex flex-col p-10 gap-10 mb-10">
                     {entrees?.map((e, i) => {
                         const selectedRecipe = mealSelections.entrees[i];
                         return (
@@ -201,7 +198,7 @@ export default function Build({
                                     })
                                 }}
                             >
-                                <MealCard 
+                                <CashierCard 
                                     name={selectedRecipe ? selectedRecipe.name : `Entree ${i + 1}`} 
                                     className={`cursor-pointer ${selection && selection.type == "Entree" && selection.num == i ? "border-yellow-300 border-3" : ""}`}
                                 />
@@ -221,7 +218,7 @@ export default function Build({
                                     })
                                 }}
                             >
-                                <MealCard 
+                                <CashierCard
                                     name={selectedRecipe ? selectedRecipe.name : `Side ${i + 1}`} 
                                     className={`cursor-pointer ${selection && selection.type == "Side" && selection.num == i ? "border-yellow-300 border-3" : ""}`}
                                 />
@@ -241,7 +238,7 @@ export default function Build({
                                     })
                                 }}
                             >
-                                <MealCard 
+                                <CashierCard 
                                     name={selectedRecipe ? selectedRecipe.name : `Drink ${i + 1}`} 
                                     className={`cursor-pointer ${selection && selection.type == "Drink" && selection.num == i ? "border-yellow-300 border-3" : ""}`}
                                 />
@@ -249,9 +246,7 @@ export default function Build({
                         );
                     })}
                 </div>
-            </div>
             <div className="w-full">
-                
                 <div className="pt-5 pl-5">
                     <h2 className=" text-3xl font-bold ">{`Select ${selection?.type} ${selection ? selection.num + 1 : ""}`}</h2>
                 </div>
@@ -262,7 +257,7 @@ export default function Build({
                             onClick={() => handleRecipeClick(item)}
                             className="cursor-pointer"
                         >
-                            <MealCard name={item.name} image="/images/image.png"/>
+                            <CashierCard name={item.name} />
                         </button>
                     ))}
                 </div>
@@ -273,6 +268,12 @@ export default function Build({
                         </Button>
                     </div>
                 )}
+            </div>
+            <div className="w-70">
+                <div className="pt-5 pl-5">
+                    <h2 className=" text-3xl font-bold ">{mealName}</h2>
+                </div>
+                
             </div>
         </div>
     );
